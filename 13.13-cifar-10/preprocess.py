8386e3a6425d99c90fd82b6bf0c9a0e8706b904e
RuntimeError <1059171929@qq.com>
2023-07-31 15:53:35 +0800

    cifar-10 acc: 0.82

---
+++ 13.13-cifar-10/preprocess.py
@@ -0,0 +1,108 @@
+import collections
+import math
+import os
+import shutil
+import torch
+import torchvision
+from torch.utils.data import Subset
+
+data_dir = '../dataset/cifar-10/'
+
+def read_csv_labels(fname):
+    """读取fname来给标签字典返回一个文件名"""
+    with open(fname, 'r') as f:
+        # 跳过文件头行(列名)
+        lines = f.readlines()[1:]
+    tokens = [l.rstrip().split(',') for l in lines]
+    return dict(((name, label) for name, label in tokens))
+
+labels = read_csv_labels(os.path.join(data_dir, 'trainLabels.csv'))
+
+#@save
+def copyfile(filename, target_dir):
+    """将文件复制到目标目录"""
+    os.makedirs(target_dir, exist_ok=True)
+    shutil.copy(filename, target_dir)
+
+#@save
+def reorg_train_valid(data_dir, labels, valid_ratio):
+    """将验证集从原始的训练集中拆分出来"""
+    # 训练数据集中样本最少的类别中的样本数
+    n = collections.Counter(labels.values()).most_common()[-1][1]
+    # 验证集中每个类别的样本数
+    n_valid_per_label = max(1, math.floor(n * valid_ratio))
+    label_count = {}
+    for train_file in os.listdir(os.path.join(data_dir, 'train')):
+        label = labels[train_file.split('.')[0]]
+        fname = os.path.join(data_dir, 'train', train_file)
+        copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                     'train_valid', label))
+        if label not in label_count or label_count[label] < n_valid_per_label:
+            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                         'valid', label))
+            label_count[label] = label_count.get(label, 0) + 1
+        else:
+            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                         'train', label))
+    return n_valid_per_label
+
+def reorg_test(data_dir):
+    """在预测期间整理测试集，以方便读取"""
+    for test_file in os.listdir(os.path.join(data_dir, 'test')):
+        copyfile(os.path.join(data_dir, 'test', test_file),
+                 os.path.join(data_dir, 'train_valid_test', 'test',
+                              'unknown'))
+
+def reorg_cifar10_data(data_dir, valid_ratio):
+    labels = read_csv_labels(os.path.join(data_dir, 'trainLabels.csv'))
+    reorg_train_valid(data_dir, labels, valid_ratio)
+    reorg_test(data_dir)
+
+batch_size = 256
+valid_ratio = 0.1
+if __name__ == '__main__':
+    reorg_cifar10_data(data_dir, valid_ratio)
+
+def load_argumentation():
+    """对图像数据集进行增广"""
+    transform_train = torchvision.transforms.Compose([
+        # 在高度和宽度上将图像放大到40像素的正方形
+        torchvision.transforms.Resize(40),
+        # 随机裁剪出一个高度和宽度均为40像素的正方形图像，
+        # 生成一个面积为原始图像面积0.64～1倍的小正方形，
+        # 然后将其缩放为高度和宽度均为32像素的正方形
+        torchvision.transforms.RandomResizedCrop(32, scale=(0.64, 1.0),
+                                                    ratio=(1.0, 1.0)),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        # 标准化图像的每个通道
+        torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
+                                        [0.2023, 0.1994, 0.2010])])
+    
+    transform_test = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
+                                        [0.2023, 0.1994, 0.2010])])
+    
+    train_ds, train_valid_ds = [torchvision.datasets.ImageFolder(
+        os.path.join(data_dir, 'train_valid_test', folder),
+        transform=transform_train) for folder in ['train', 'train_valid']]
+
+    valid_ds, test_ds = [torchvision.datasets.ImageFolder(
+        os.path.join(data_dir, 'train_valid_test', folder),
+        transform=transform_test) for folder in ['valid', 'test']]
+
+    train_length = 45000
+    print(f'训练样本为前{train_length}个。')
+    train_iter, train_valid_iter = [torch.utils.data.DataLoader(
+        dataset, batch_size, shuffle=True, drop_last=True, num_workers = 4)
+        for dataset in (Subset(train_ds, range(train_length)), train_valid_ds)]
+    
+
+    valid_iter = torch.utils.data.DataLoader(valid_ds, batch_size, shuffle=False,
+                                            drop_last=True, num_workers = 4)
+
+    test_iter = torch.utils.data.DataLoader(test_ds, batch_size, shuffle=False,
+                                            drop_last=False, num_workers = 4)
+    
+    return train_iter, train_valid_iter, valid_iter, test_iter, train_valid_ds, test_ds
